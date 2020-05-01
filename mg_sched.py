5f6e55148fd28da43bf172c90bc4e636debca72f
uttie <uttieg@gmail.com>
2020-04-30 19:04:27 -0700

    add schedule and stubbed in cli commands for mg_sched

--- mg_sched.py
+++ mg_sched.py
@@ -6,10 +6,14 @@ that Megatools downloader should run at.
 
 import sys
 import subprocess
+import configparser
 
+import click
 import schedule
 
 MEGA_PROC = "megatools"
+CONFIG_FILE = "sched.ini"
+CONFIG_PROFILE = "normal"
 
 def pkill(process_name, signal=0):
     """ emulate *nix pkill command """
@@ -26,13 +30,46 @@ def validate():
     if pkill(MEGA_PROC):
         print("Megatools cli is not running.")
         sys.exit(1)
+    return True
 
-def main():
-    """ your mother
-    """
+@click.group()
+def cli():
+    """ dummy function for click argument parsing """
+    return
 
-    validate()
+@cli.command(name='new', help="New schedule entry")
+def new_sched_entry():
+    """ new schedule entry """
 
+    click.echo("new")
+
+@cli.command(name='list', help="List schedule entries")
+def list_sched():
+    """ list schedule entries """
+
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+
+    print()
+    print("Time", "\t", "Speed")
+    print("====", "\t", "=====")
+    for (k,v) in config[CONFIG_PROFILE].items():
+        print(k, "\t", v)
+    print()
+
+@cli.command(name='edit', help="Edit schedule entry")
+def edit_sched():
+    """ edit schedule entry """
+
+    click.echo("edit")
+
+@cli.command(name='remove', help="Delete schedule entry")
+def delete_schedule():
+    """ delete schedule entry """
+
+    click.echo("remove")
 
 if __name__ == "__main__":
-    main()
+    # validate config
+    #validate()
+    cli()
