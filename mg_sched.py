cec641f318e729834a70ee323767c1eef64ba552
uttie <uttieg@gmail.com>
2020-04-30 23:45:43 -0700

    lots of changes. got scheduler run/stop working.

--- mg_sched.py
+++ mg_sched.py
@@ -1,38 +1,90 @@
 #!/usr/bin/env python
-# pylint: disable=W0703
+# pylint: disable=W0703,C0103
 
 """
 This module is to manage a schedule of times and speeds
 that Megatools downloader should run at.
 """
 
+import os
 import sys
+import time
+import signal
 import subprocess
 import configparser
 
 import click
 import schedule
 
+from daemonize import Daemonize
+
+PROC_NAME = __file__[:-3]
 MEGA_PROC = "megatools"
 CONFIG_FILE = "sched.ini"
 CONFIG_PROFILE = "normal"
+DAEMON_PIDFILE = "/tmp/%s.pid" % PROC_NAME
+DAEMON_PIPE = "/tmp/.%s.pipe" % MEGA_PROC
 
-def pkill(process_name, signal=0):
+def kill(process, signo=0, cat="name"):
     """ emulate *nix pkill command """
 
-    proc = subprocess.Popen(['pkill', '-%d'%signal, process_name],
+    cmd = "kill"
+    if cat == "name":
+        cmd = "pkill"
+
+    #import pdb; pdb.set_trace()
+    proc = subprocess.Popen([cmd, '-%d'%signo, process],
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     proc.communicate()
     return proc.returncode
+
+def check_pidfile(pidfile=DAEMON_PIDFILE):
+    """ Check For the existence of a unix pid. """
+
+    try:
+        with open(pidfile, 'r') as filehandle:
+            return kill(filehandle.read(), cat="pid") == 0
+    except ValueError:
+        return False
+    except OSError:
+        return False
+    else:
+        return False
 
 def validate():
     """ validate configs """
 
     # Check if megatools is running
-    if pkill(MEGA_PROC):
+    if kill(MEGA_PROC):
         print("Megatools cli is not running.")
         sys.exit(1)
     return True
+
+def run_schedules():
+    """ run evertyhing """
+
+    bail = False
+
+    def sig_handler(signo, frame):
+        msg = ("Caught signal: %d | frame: " % signo)  + str(frame)
+        print(msg)
+        if signo == signal.SIGWINCH.value:
+            msg += " WICNCHYYY!!!! "
+        with open(DAEMON_PIPE, 'w') as pipe:
+            pipe.write(msg+"\n")
+
+        if signo == signal.SIGHUP.value:
+            bail = True
+
+
+    signal.signal(signal.SIGUSR1, sig_handler)
+    signal.signal(signal.SIGHUP, sig_handler)
+    signal.signal(signal.SIGWINCH, sig_handler)
+    while not bail:
+        time.sleep(1)
+    with open(DAEMON_PIPE, 'w') as pipe:
+        pipe.write("done")
+    print('done')
 
 #   ____ _     ___                      _
 #  / ___| |   |_ _|   ___ _ __ ___   __| |___
@@ -40,7 +92,7 @@ def validate():
 # | |___| |___ | |  | (__| | | | | | (_| \__ \
 #  \____|_____|___|  \___|_| |_| |_|\__,_|___/
 
-@click.group()
+@click.group(name="CLI", help="Manage Megatools downloader schedule")
 def cli():
     """ dummy function for click argument parsing """
     return
@@ -113,6 +165,38 @@ def delete_schedule(time):
         config.write(filehandle)
     click.echo("Removed entry for time: %s" % time)
 
+
+@click.group(name="daemon", help="yo mamma")
+def daemon_ctl():
+    pass
+
+@cli.command(name="run", help="start the scheduler")
+def daemon_run():
+    """ Start the scheduling daemon """
+ 
+    if daemon_status:
+        click.echo("> Scheduler already running.")
+        return
+    click.echo("> Trying to start daemon")
+    daemon = Daemonize(app="test_app", pid=DAEMON_PIDFILE,
+                       action=run_schedules)
+    daemon.start()
+
+@cli.command(name="stop", help="stop the scheduler")
+def daemon_stop():
+    """ Stop the scheduling daemon """
+ 
+    ret = None
+    if not daemon_status:
+        click.echo("> Scheduler not running.")
+        return
+
+    # try stopping it
+    with open(DAEMON_PIDFILE, 'r') as filehandle:
+        error = kill(filehandle.read(), signo=signal.SIGHUP, cat="pid") != 0
+    if error:
+        click.echo("Couldn't stop scheduler.")
+
 #                  _
 #  _ __ ___   __ _(_)_ __
 # | '_ ` _ \ / _` | | '_ \
@@ -120,6 +204,40 @@ def delete_schedule(time):
 # |_| |_| |_|\__,_|_|_| |_|
 
 if __name__ == "__main__":
+    click.clear()
     # validate config
     #validate()
+    megatools_status = None
+    daemon_status = check_pidfile()
+
+    # TODO: Display status: Red/Green Icons?
+    print("""
+##############################################################
+#                                         _              _   #
+#   _ __ ___   ___  __ _  __ _   ___  ___| |__   ___  __| |  #
+#  | '_ ` _ \ / _ \/ _` |/ _` | / __|/ __| '_ \ / _ \/ _` |  #
+#  | | | | | |  __/ (_| | (_| | \__ \ (__| | | |  __/ (_| |  #
+#  |_| |_| |_|\___|\__, |\__,_| |___/\___|_| |_|\___|\__,_|  #
+#                  |___/                                     #
+#                                                            #""")
+    print("# MEGA:  ", megatools_status)
+    print("# DAEMON:", daemon_status)
+    print("##############################################################\n")
+
+#    if not daemon_status:
+#        try:
+#            pid = os.fork()
+#        except OSError as exc:
+#            print("Error forking: ", exc)
+#        # child
+#        if pid == 0:
+#            print("> Trying to start daemon")
+#            daemon = Daemonize(app="test_app", pid=DAEMON_PIDFILE,
+#                               action=run_schedules)
+#            daemon.start()
+#            print("# DAEMON:", check_pidfile())
+#            # doesn't seem to get here
+#        # parent is business as usual
+
+    # do cli stuff
     cli()
