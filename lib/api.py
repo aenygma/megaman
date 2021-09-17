f9582941ce9f356dc4301bb2e67f48f8449771c7
uttie <uttieg@gmail.com>
2021-09-17 13:25:35 -0700

    add api to add entry to hisotry db

--- lib/api.py
+++ lib/api.py
@@ -3,18 +3,43 @@
 #
 
 import tasks
+from models import Entries
 
-def add_item(url):
+def init():
+    """
+    generic setup stuff
+    """
+
+    # create db tables if not exist
+    Entries.create_table()
+
+
+def _entry_done(item_id):
+    """
+    database helper to set completed to 1
+    """
+
+    entry = Entries.get(item_id)
+    entry.completed = 1
+    entry.save()
+
+def add_item(url, name):
     """
     add a item to download
     """
 
     # add it to history db
-    item_id = history_db.add(url)
+    entry = entries.create(
+                url = url,
+                name = kwargs.get("name", None),
+                completed = 0,
+                inprogress = 0,
+                refurl = kwargs.get("refurl", None))
 
     # start consumer
     # XXX: set done iff no failures or errors
-    pipeline = tasks.start_consumer.s(url).then(history_db.done, item_id)
+    pipeline = tasks.start_consumer.s(url).then(
+                _entry_done, item_id)
 
     # set task status to done in history db
     #  set blocking or add this as callback
